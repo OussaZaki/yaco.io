1f29c3d425d68652c2f46bd28e7b1552e564ee84
Oussama Zaki <oussama.zaki@klarna.com>
2018-01-23 18:32:37 +0100

    fixes peerjs hostname

--- src/core/onBoard/hub/index.js
+++ src/core/onBoard/hub/index.js
@@ -1,6 +1,6 @@
 import Peer from 'peerjs'
 
 export function initialize () {
-    const peer = new Peer({host: 'http://yacohub-live.eu-west-1.elasticbeanstalk.com/', port: 9000, path: '/conference-hub', debug: true})
+    const peer = new Peer({host: 'yacohub-live.eu-west-1.elasticbeanstalk.com/', port: 9000, path: '/conference-hub', debug: true})
     return peer
 }
